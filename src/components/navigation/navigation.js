57c6cfe4546f7d42ac108e367c8a3515771af51e
Erin Trainor <trainor.erin@gmail.com>
2019-08-09 03:34:33 -0700

    media query for nav 414px complete

--- src/components/navigation/navigation.js
+++ src/components/navigation/navigation.js
@@ -1,47 +1,62 @@
 import React from "react"
 import "../navigation/navigation.scss"
 
-// function toggleMenu() {
-// 	document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
-// }
+class Navigation extends React.Component {
+  constructor(props) {
+    super(props);
+      this.toggleNavbar = this.toggleNavbar.bind(this);
+      this.state = {
+        collapsed: true,
+      };
+  }
 
-function Navigation () {
-  return (
-    <nav>
-      <ul className="navigation">
-        {/* <li><a title="Menu" onclick="toggleMenu()">☰ Menu</a></li> */}
-        {/* <li><a href="/">Home</a></li> */}
-        <li><a href="/about-us">About</a></li>
-        {/* <li><a href="form.html">Form</a></li> */}
-        {/* <li><a href="https://httpstatusdogs.com/img/307.jpg">How it Works</a></li> */}
-        {/* <li><a href="https://httpstatusdogs.com/img/307.jpg">Testimonials</a></li> */}
-        <li>Services
-          <ul className="dropdown-menu">
-            <li><a href="/advisory-services">Advisory</a></li>
-            <li><a href="/advocacy-services">Advocacy</a></li>
-          </ul>
-        </li>
+  toggleNavbar() {
+    this.setState({
+      collapsed: !this.state.collapsed,
+    });
+  }
+  render() {
+    const classDropdownMenu = 'navbar-collapse' + (this.state.collapsed ? ' collapse' : '')
 
-        <li>Resources
-          <ul className="dropdown-menu">
-            <li><a 
-            //TODO: eventually set this form link up as a modal
-            href="https://www.dropbox.com/s/f7e4oiu3lkzgdiu/New%20Client%20Information.docx?dl=0" target="_blank">New Client Form</a></li>
-            <li><a href="/resources">Recommended Reading</a></li>
-            {/* <li><a href="/blog">Blog</a></li> */}
+    return (
+      <>
+        <div className="hamburger">
+          <button onClick={this.toggleNavbar}>
+            <span className="navbar-toggler-icon">☰ Menu</span>
+          </button>
+        </div>
+        <nav className={classDropdownMenu} id="navbarSupportedContent">
+          <ul className="navigation">
+            <li><a href="/about-us">About</a></li>
+            <li>Services
+              <ul className="dropdown-menu">
+                <li><a href="/advisory-services">Advisory</a></li>
+                <li><a href="/advocacy-services">Advocacy</a></li>
+              </ul>
+            </li>
+
+            <li>Resources
+              <ul className="dropdown-menu">
+                <li><a 
+                //TODO: eventually set this form link up as a modal
+                href="https://www.dropbox.com/s/f7e4oiu3lkzgdiu/New%20Client%20Information.docx?dl=0" target="_blank">New Client Form</a></li>
+                <li><a href="/resources">Recommended Reading</a></li>
+              </ul>
+            </li>
+            {/* <li>News
+              <ul className="dropdown-menu">
+                <li><a href="/advisory-services">Blog</a></li>
+              </ul>
+            </li> */}
+            {/* <li><a href="https://httpstatusdogs.com/img/404.jpg">Blog</a></li> */}
+            {/* <li><a href="https://httpstatusdogs.com/img/307.jpg">Client Portal</a></li> */}
+            <li><a href="/contact-form">Contact</a></li>
           </ul>
-        </li>
-        {/* <li>News
-          <ul className="dropdown-menu">
-            <li><a href="/advisory-services">Blog</a></li>
-          </ul>
-        </li> */}
-        {/* <li><a href="https://httpstatusdogs.com/img/404.jpg">Blog</a></li> */}
-        {/* <li><a href="https://httpstatusdogs.com/img/307.jpg">Client Portal</a></li> */}
-        <li><a href="/contact-form">Contact</a></li>
-      </ul>
-    </nav>
-  )
+        </nav>
+      </>
+    )
+  }
 }
 
-export default Navigation //toggleMenu;
+export default Navigation
+
